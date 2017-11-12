052116133344934e743e2812bdf30e2d5feb840d
Andrew Hyatt <ahyattdev@icloud.com>
2017-11-11 21:56:56 -0500

    Add more hate variations

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
+document.body.innerHTML = document.body.innerHTML.replace(/hated/g, 'loved 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hater/g, 'lover 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hating/g, 'loving 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hatred/g, 'love 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hate crime/g, 'love hug 💗');
 document.body.innerHTML = document.body.innerHTML.replace(/hate/g, 'love 💗');
