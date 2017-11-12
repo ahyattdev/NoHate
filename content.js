d98c1a203c019afccaf5ec7798d0981441c0c939
Andrew Hyatt <ahyattdev@icloud.com>
2017-11-11 22:09:19 -0500

    Replace hatred of all cases

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-document.body.innerHTML = document.body.innerHTML.replace(/hated/g, 'loved 💗');
-document.body.innerHTML = document.body.innerHTML.replace(/hater/g, 'lover 💗');
-document.body.innerHTML = document.body.innerHTML.replace(/hating/g, 'loving 💗');
-document.body.innerHTML = document.body.innerHTML.replace(/hatred/g, 'love 💗');
-document.body.innerHTML = document.body.innerHTML.replace(/hate crime/g, 'love hug 💗');
-document.body.innerHTML = document.body.innerHTML.replace(/hate/g, 'love 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hated/gi, 'loved 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hater/gi, 'lover 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hating/gi, 'loving 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hatred/gi, 'love 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hate crime/gi, 'love hug 💗');
+document.body.innerHTML = document.body.innerHTML.replace(/hate/gi, 'love 💗');
